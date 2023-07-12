70f4ad8226eb1f74465cc0d58881e6d2f0c9d98c
Tal Hazi <thazi@cisco.com>
2023-07-12 12:06:18 +0100

    fix typo

--- provider/examples/resources/ios/ios_example.tf
+++ provider/examples/resources/ios/ios_example.tf
@@ -14,7 +14,7 @@ provider "cdo" {
 resource "cdo_ios_device" "my_ios" {
   name     = "my_ios"
   sdc_type = "SDC"
-  sdc_uid = "<FILL_ME>"
+  sdc_uid  = "<FILL_ME>"
   ipv4     = "<FILL_ME>"
   username = "<FILL_ME>"
   password = "<FILL_ME>"

f8ca719f5a09e6526a81ef404fa34fccc266be82
Weilue Luo <weilluo@cisco.com>
2023-08-14 17:19:27 +0100

    provider skeleton

---
+++ provider/internal/device/ftd/operation.go
@@ -0,0 +1,43 @@
+package ftd
+
+import (
+	"context"
+)
+
+func Read(ctx context.Context, resource *Resource, stateData *ResourceModel) error {
+	// TODO: fill me
+
+	// do read
+
+	// map return struct to sdc model
+
+	return nil
+}
+
+func Create(ctx context.Context, resource *Resource, planData *ResourceModel) error {
+	// TODO: fill me
+
+	// do create
+
+	// map return struct to sdc model
+
+	return nil
+}
+
+func Update(ctx context.Context, resource *Resource, planData *ResourceModel, stateData *ResourceModel) error {
+	// TODO: fill me
+
+	// do update
+
+	// map return struct to sdc model
+
+	return nil
+}
+
+func Delete(ctx context.Context, resource *Resource, stateData *ResourceModel) error {
+	// TODO: fill me
+
+	// do delete
+
+	return nil
+}

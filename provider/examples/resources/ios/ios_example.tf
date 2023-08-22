83ba1e08c20cdfcd32b9483578e14ecccc564f2e
Tal Hazi <thazi@cisco.com>
2023-08-22 18:00:34 +0300

    update base url example

--- provider/examples/resources/ios/ios_example.tf
+++ provider/examples/resources/ios/ios_example.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "cdo" {
-  base_url  = "https://staging.dev.lockhart.io"
+  base_url  = "<https://www.defenseorchestrator.com|https://www.defenseorchestrator.eu|https://apj.cdo.cisco.com>"
   api_token = "<replace-with-api-token-generated-from-cdo>"
 }
 

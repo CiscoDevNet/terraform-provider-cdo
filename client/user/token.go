18244a2208c028b19e05e1191c0e09404642631a
Siddhu Warrier <siwarrie@cisco.com>
2023-09-21 23:06:22 +0000

    fix(LH-70915): Use external-compute tokens to generate bootstrap data
    There is an Anubis weirdness by which only a single token can exist for a given scope (external-compute and fmc tokens are exceptions we deliberately engineered to work around this limitation which is a feature of some kind). So when you call `/oauth/token` with the API token fed in to the Terraform provider, you actually get back the same token you called with. When this token is passed into the bootstrap function of our SDC, the bootstrap method revokes the token after setting up the SDC. This nvalidates the token the terraform-provider is using, and we end up having to regenerate the token. In this PR, I switch to using the external-compute token instead for bootstrapping the SDC. (yes, this is a hack, and no Weilue, we are not going to "fix" this, there have been many discussions around this between Doron and me for months now :) ).

--- client/user/token.go
+++ client/user/token.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+
 	"github.com/CiscoDevnet/terraform-provider-cdo/go-client/model/user/auth"
 
 	"github.com/CiscoDevnet/terraform-provider-cdo/go-client/internal/http"
@@ -19,6 +20,25 @@ func NewGetTokenInput() GetTokenInput {
 func GetToken(ctx context.Context, client http.Client, getTokenInp GetTokenInput) (GetTokenOutput, error) {
 
 	url := url.UserToken(client.BaseUrl())
+
+	req := client.NewPost(ctx, url, nil)
+
+	var getTokenOutp GetTokenOutput
+	if err := req.Send(&getTokenOutp); err != nil {
+		return GetTokenOutput{}, err
+	}
+
+	return getTokenOutp, nil
+}
+
+/*
+* This function will generate a new external-compute token. We use the external compute token to generate the CDO bootstrap data, because
+* an Anubis weirdness (which I have been assured is a feature, not a bug) results in calling `/token` returning the same token that it
+* was called with, because we allow only one token to exist for a given scope.
+ */
+func GetExternalComputeToken(ctx context.Context, client http.Client, getTokenInp GetTokenInput) (GetTokenOutput, error) {
+
+	url := url.ExternalComputeToken(client.BaseUrl())
 
 	req := client.NewPost(ctx, url, nil)
 

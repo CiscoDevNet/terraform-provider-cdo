e5988e252b9a7b02d062767ab2e0eae2224f72b0
Weilue Luo <weilluo@cisco.com>
2023-09-22 16:41:24 +0100

    feat(LH-69564): Supports ASA Onboarding Using New Model (#54)

--- client/model/user/auth/info.go
+++ client/model/user/auth/info.go
@@ -1,6 +1,12 @@
 package auth
 
-import "github.com/CiscoDevnet/terraform-provider-cdo/go-client/model/user/auth/role"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/CiscoDevnet/terraform-provider-cdo/go-client/model/featureflag"
+	"github.com/CiscoDevnet/terraform-provider-cdo/go-client/model/user/auth/role"
+	"strings"
+)
 
 type Info struct {
 	UserAuthentication Authentication `json:"userAuthentication"`
@@ -27,4 +33,27 @@ type Details struct {
 	TenantUserRoles        string `json:"TenantUserRoles"`
 	TenantDatabaseName     string `json:"TenantDatabaseName"`
 	TenantPayType          string `json:"TenantPayType"`
+}
+
+func (info *Info) HasFeatureFlagEnabled(featureFlag featureflag.Type) bool {
+	featureMap := info.getFeatureMap()
+	enabled, ok := featureMap[featureFlag.String()]
+	return ok && enabled
+}
+
+func (info *Info) getFeatureMap() map[string]bool {
+	featureMap := map[string]bool{}
+	err := json.Unmarshal([]byte(info.UserAuthentication.Details.TenantDbFeatures), &featureMap)
+	if err != nil {
+		// feature flag received is not a json
+		panic(fmt.Sprintf("feature flag received from authentication service is not in valid format: %s", info.UserAuthentication.Details.TenantDbFeatures))
+	}
+	// convert feature flag keys to lowercase
+	// TODO: make use of lh-feature
+	normalizedFeatureMap := map[string]bool{}
+	for k, v := range featureMap {
+		normalizedFeatureMap[strings.ToLower(k)] = v
+	}
+
+	return normalizedFeatureMap
 }

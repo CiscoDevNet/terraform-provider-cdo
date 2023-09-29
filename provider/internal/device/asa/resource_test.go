5e884cfba3a63b52467fceafa83bd25a1d6f3856
Siddhu Warrier <siwarrie@cisco.com>
2023-09-29 22:44:56 +0100

    fix(lh-71071): use resources spun up using Terraform for tests (#66)
    * fix(lh-71071): use resources spun up using Terraform for tests
    
    * fix(lh-71029): fix a few issues
    
    * Fix test
    
    * Re-enable acceptance tests only for main
    
    * Add spun up iOS resource and test
    
    * Re-enable acceptance tests only for main

--- provider/internal/device/asa/resource_test.go
+++ provider/internal/device/asa/resource_test.go
@@ -1,10 +1,11 @@
 package asa_test
 
 import (
-	"github.com/CiscoDevnet/terraform-provider-cdo/internal/util/testutil"
 	"regexp"
 	"strconv"
 	"testing"
+
+	"github.com/CiscoDevnet/terraform-provider-cdo/internal/util/testutil"
 
 	"github.com/CiscoDevnet/terraform-provider-cdo/internal/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -53,8 +54,6 @@ var testAsaResource_SDC = testAsaResourceType{
 	Port: acctest.Env.AsaResourceSdcPort(),
 }
 
-var alternativeDeviceLocation = acctest.Env.AsaResourceAlternativeDeviceLocation()
-
 var testAsaResourceConfig_SDC = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_SDC)
 
 // new name config.
@@ -68,42 +67,6 @@ var testAsaResource_SDC_BadCreds = acctest.MustOverrideFields(testAsaResource_SD
 	"Password": acctest.Env.AsaResourceSdcWrongPassword(),
 })
 var testAsaResourceConfig_SDC_NewCreds = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_SDC_BadCreds)
-
-var testAsaResource_SDC_NewLocation = acctest.MustOverrideFields(testAsaResource_SDC, map[string]any{"SocketAddress": alternativeDeviceLocation})
-var testAsaResourceConfig_SDC_NewLocation = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_SDC_NewLocation)
-
-// CDG configs
-
-// default config.
-var testAsaResource_CDG = testAsaResourceType{
-	Name:              acctest.Env.AsaResourceCdgName(),
-	SocketAddress:     acctest.Env.AsaResourceCdgSocketAddress(),
-	ConnectorName:     acctest.Env.AsaResourceCdgConnectorName(),
-	ConnectorType:     acctest.Env.AsaResourceCdgConnectorType(),
-	Username:          acctest.Env.AsaResourceCdgUsername(),
-	Password:          acctest.Env.AsaResourceCdgPassword(),
-	IgnoreCertificate: acctest.Env.AsaResourceCdgIgnoreCertificate(),
-	Labels:            acctest.Env.AsaResourceCdgTags().GetLabelsJsonArrayString(),
-
-	Host: acctest.Env.AsaResourceCdgHost(),
-	Port: acctest.Env.AsaResourceCdgPort(),
-}
-var testAsaResourceConfig_CDG = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_CDG)
-
-// new name config.
-var testAsaResource_CDG_NewName = acctest.MustOverrideFields(testAsaResource_CDG, map[string]any{
-	"Name": acctest.Env.AsaResourceCdgNewName(),
-})
-var testAsaResourceConfig_CDG_NewName = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_CDG_NewName)
-
-// bad credentials config.
-var testAsaResource_CDG_BadCreds = acctest.MustOverrideFields(testAsaResource_CDG, map[string]any{
-	"Password": acctest.Env.AsaResourceCdgWrongPassword(),
-})
-var testAsaResourceConfig_CDG_NewCreds = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_CDG_BadCreds)
-
-var testAsaResource_CDG_NewLocation = acctest.MustOverrideFields(testAsaResource_CDG, map[string]any{"SocketAddress": alternativeDeviceLocation})
-var testAsaResourceConfig_CDG_NewLocation = acctest.MustParseTemplate(asaResourceTemplate, testAsaResource_CDG_NewLocation)
 
 func TestAccAsaDeviceResource_SDC(t *testing.T) {
 	resource.Test(t, resource.TestCase{
@@ -151,72 +114,16 @@ func TestAccAsaDeviceResource_SDC(t *testing.T) {
 					resource.TestCheckResourceAttr("cdo_asa_device.test", "password", testAsaResource_SDC.Password),
 				),
 			},
-			// change location test
-			{
-				Config: acctest.ProviderConfig() + testAsaResourceConfig_SDC_NewLocation,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "socket_address", testAsaResource_SDC_NewLocation.SocketAddress),
-				),
-			},
+			// change location test - disabled until we create another asa
+			// {
+			// 	Config: acctest.ProviderConfig() + testAsaResourceConfig_SDC_NewLocation,
+			// 	Check: resource.ComposeAggregateTestCheckFunc(
+			// 		resource.TestCheckResourceAttr("cdo_asa_device.test", "socket_address", testAsaResource_SDC_NewLocation.SocketAddress),
+			// 	),
+			// },
 			// Delete testing automatically occurs in TestCase
 		},
 	})
 }
 
-func TestAccAsaDeviceResource_CDG(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 acctest.PreCheckFunc(t),
-		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Create and Read testing
-			{
-				Config: acctest.ProviderConfig() + testAsaResourceConfig_CDG,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "name", testAsaResource_CDG.Name),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "socket_address", testAsaResource_CDG.SocketAddress),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "host", testAsaResource_CDG.Host),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "port", strconv.FormatInt(testAsaResource_CDG.Port, 10)),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "connector_type", testAsaResource_CDG.ConnectorType),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "username", testAsaResource_CDG.Username),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "password", testAsaResource_CDG.Password),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "labels.#", strconv.Itoa(len(acctest.Env.AsaResourceCdgTags().Labels))),
-					resource.TestCheckResourceAttrWith("cdo_asa_device.test", "labels.0", testutil.CheckEqual(acctest.Env.AsaResourceCdgTags().Labels[0])),
-					resource.TestCheckResourceAttrWith("cdo_asa_device.test", "labels.1", testutil.CheckEqual(acctest.Env.AsaResourceCdgTags().Labels[1])),
-					resource.TestCheckResourceAttrWith("cdo_asa_device.test", "labels.2", testutil.CheckEqual(acctest.Env.AsaResourceCdgTags().Labels[2])),
-				),
-			},
-			// Update and Read testing
-			{
-				Config: acctest.ProviderConfig() + testAsaResourceConfig_CDG_NewName,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "name", testAsaResource_CDG_NewName.Name),
-				),
-			},
-			// bad credentials tests
-			{
-				Config: acctest.ProviderConfig() + testAsaResourceConfig_CDG_NewCreds,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "name", testAsaResource_CDG_BadCreds.Name),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "password", testAsaResource_CDG_BadCreds.Password),
-				),
-				ExpectError: regexp.MustCompile(".*bad credentials.*"),
-			},
-			// fix bad credentials test
-			{
-				Config: acctest.ProviderConfig() + testAsaResourceConfig_CDG,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "name", testAsaResource_CDG.Name),
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "password", testAsaResource_CDG.Password),
-				),
-			},
-			// change location test
-			{
-				Config: acctest.ProviderConfig() + testAsaResourceConfig_CDG_NewLocation,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cdo_asa_device.test", "socket_address", testAsaResource_CDG_NewLocation.SocketAddress),
-				),
-			},
-			// Delete testing automatically occurs in TestCase
-		},
-	})
-}
+// removed CDG test as we do not want to be using ASAs accessible from the public subnet for our tests

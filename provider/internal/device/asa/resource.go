951f38e885d618b07b120ffdba125fd499285ea0
Weilue Luo <weilluo@cisco.com>
2023-10-02 14:24:57 +0100

    fix(LH-71207): Do not fail to onboard device if labels are not specified (#68)

--- provider/internal/device/asa/resource.go
+++ provider/internal/device/asa/resource.go
@@ -6,6 +6,8 @@ import (
 	"github.com/CiscoDevnet/terraform-provider-cdo/go-client/model/device/tags"
 	"github.com/CiscoDevnet/terraform-provider-cdo/internal/util"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
 	"strconv"
 	"strings"
 
@@ -109,7 +111,9 @@ func (r *AsaDeviceResource) Schema(ctx context.Context, req resource.SchemaReque
 			"labels": schema.ListAttribute{
 				MarkdownDescription: "Set a list of labels to identify the device as part of a group. Refer to the [CDO documentation](https://docs.defenseorchestrator.com/t-applying-labels-to-devices-and-objects.html#!c-labels-and-filtering.html) for details on how labels are used in CDO.",
 				Optional:            true,
+				Computed:            true,
 				ElementType:         types.StringType,
+				Default:             listdefault.StaticValue(types.ListValueMust(types.StringType, []attr.Value{})), // default to empty list
 				Validators: []validator.List{
 					listvalidator.UniqueValues(),
 				},

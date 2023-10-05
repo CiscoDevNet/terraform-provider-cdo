eea48b2e60b69da816015cb550889b81ae5ffd91
Weilue Luo <weilluo@cisco.com>
2023-10-05 10:34:44 +0100

    add examples

---
+++ provider/internal/examples/resource.go
@@ -0,0 +1,157 @@
+package examples
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	cdoClient "github.com/CiscoDevnet/terraform-provider-cdo/go-client"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+var _ resource.Resource = &ExampleResource{}
+
+func NewExampleResource() resource.Resource {
+	return &ExampleResource{}
+}
+
+type ExampleResource struct {
+	client *cdoClient.Client
+}
+
+type ExampleResourceModel struct {
+	Id   types.String `tfsdk:"id"`
+	Name types.String `tfsdk:"name"`
+}
+
+func (r *ExampleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_example"
+}
+
+func (r *ExampleResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Example resource description.",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "Example Id description",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "Example name description",
+				Required:            true,
+			},
+		},
+	}
+}
+
+func (r *ExampleResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*cdoClient.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *cdoClient.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *ExampleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+
+	tflog.Trace(ctx, "read Example resource")
+
+	// 1. read state data
+	var stateData ExampleResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &stateData)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// 2. do read
+	if err := Read(ctx, r, &stateData); err != nil {
+		resp.Diagnostics.AddError("failed to read Example resource", err.Error())
+	}
+
+	// 3. save data into terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &stateData)...)
+}
+
+func (r *ExampleResource) Create(ctx context.Context, req resource.CreateRequest, res *resource.CreateResponse) {
+
+	tflog.Trace(ctx, "create Example resource")
+
+	// 1. read plan data into planData
+	var planData ExampleResourceModel
+	res.Diagnostics.Append(req.Plan.Get(ctx, &planData)...)
+	if res.Diagnostics.HasError() {
+		return
+	}
+
+	// 2. use plan data to create device and fill up rest of the model
+	if err := Create(ctx, r, &planData); err != nil {
+		res.Diagnostics.AddError("failed to create Example resource", err.Error())
+		return
+	}
+
+	// 3. set state using filled model
+	res.Diagnostics.Append(res.State.Set(ctx, &planData)...)
+}
+
+func (r *ExampleResource) Update(ctx context.Context, req resource.UpdateRequest, res *resource.UpdateResponse) {
+
+	tflog.Trace(ctx, "update Example resource")
+
+	// 1. read plan data
+	var planData ExampleResourceModel
+	res.Diagnostics.Append(req.Plan.Get(ctx, &planData)...)
+	if res.Diagnostics.HasError() {
+		return
+	}
+
+	// 2. read state data
+	var stateData ExampleResourceModel
+	res.Diagnostics.Append(req.State.Get(ctx, &stateData)...)
+	if res.Diagnostics.HasError() {
+		return
+	}
+
+	// 3. do update
+	if err := Update(ctx, r, &planData, &stateData); err != nil {
+		res.Diagnostics.AddError("failed to update Example resource", err.Error())
+	}
+
+	// 4. set resulting state
+	res.Diagnostics.Append(res.State.Set(ctx, &stateData)...)
+}
+
+func (r *ExampleResource) Delete(ctx context.Context, req resource.DeleteRequest, res *resource.DeleteResponse) {
+
+	tflog.Trace(ctx, "delete Example resource")
+
+	var stateData ExampleResourceModel
+	res.Diagnostics.Append(req.State.Get(ctx, &stateData)...)
+	if res.Diagnostics.HasError() {
+		return
+	}
+
+	if err := Delete(ctx, r, &stateData); err != nil {
+		res.Diagnostics.AddError("failed to delete Example resource", err.Error())
+	}
+}

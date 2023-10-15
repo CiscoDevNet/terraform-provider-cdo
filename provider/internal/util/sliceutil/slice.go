5e028166a0106abb628133eff4a054458abbe07b
Weilue Luo <weilluo@cisco.com>
2023-10-15 18:29:36 +0100

    cleanup

--- provider/internal/util/sliceutil/slice.go
+++ provider/internal/util/sliceutil/slice.go
@@ -2,7 +2,6 @@ package sliceutil
 
 import (
 	"github.com/CiscoDevnet/terraform-provider-cdo/internal/util/goutil"
-	"sort"
 )
 
 // Map takes a slice and a function, apply the function to each element in the slice, and return the mapped slice.
@@ -27,14 +26,6 @@ func MapWithError[T any, V any](sliceT []T, mapFunc func(T) (V, error)) ([]V, er
 
 	}
 	return sliceV, nil
-}
-
-// SortStrings is a non-in-place version of sort.Strings
-func SortStrings(toSort []string) []string {
-	temp := make([]string, len(toSort))
-	copy(temp, toSort)
-	sort.Strings(temp)
-	return temp
 }
 
 func StringsEqual(s1 []string, s2 []string) bool {

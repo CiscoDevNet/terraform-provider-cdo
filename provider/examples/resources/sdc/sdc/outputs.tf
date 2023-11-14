2ed72f2fe68d0852219ae48b55694dec7b76e56e
Weilue Luo <weilluo@cisco.com>
2023-11-14 17:40:41 +0000

    remove sdc example

--- provider/examples/resources/sdc/sdc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sdc_name" {
-  value       = cdo_sdc.example.name
-  description = "The name of the SDC spun up in AWS."
-}
-
-output "sdc_bootstrap_data" {
-  value       = cdo_sdc.example.bootstrap_data
-  description = "The bootstrap data of the SDC spun up in AWS."
-}

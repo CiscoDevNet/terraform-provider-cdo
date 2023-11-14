b18cac1294aeaf2313c1394c571586ecdc4d79d9
Weilue Luo <weilluo@cisco.com>
2023-11-14 17:41:37 +0000

    remove sdc example

--- provider/examples/resources/sdc/vpc/vpc.tf
+++
@@ -1,9 +0,0 @@
-# VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.combined_subnet_ranges["VPC"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name : "${var.resource_prefix}-vpc"
-  }
-}

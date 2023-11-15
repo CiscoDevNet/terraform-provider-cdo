9288f6d74bd8b1eb77bf6059f90d965ae004aa50
Weilue Luo <weilluo@cisco.com>
2023-11-15 14:40:58 +0000

    add hostname output

--- provider/internal/cdfmc/operation.go
+++ provider/internal/cdfmc/operation.go
@@ -9,17 +9,15 @@ import (
 func Read(ctx context.Context, resource *Resource, stateData *ResourceModel) error {
 
 	// do read
-	// e.g. readOutp, err := resource.client.ReadExample(ctx, ...)
-
 	readOut, err := resource.client.ReadCloudFmcDevice(ctx)
 	if err != nil {
 		return err
 	}
 
 	// map response to terraform types
-	// e.g. stateData.ID = types.StringValue(readOutp.Uid)
 	stateData.Id = types.StringValue(readOut.Uid)
 	stateData.Name = types.StringValue(readOut.Name)
+	stateData.Hostname = types.StringValue(readOut.Host)
 
 	return nil
 }
@@ -27,17 +25,15 @@ func Read(ctx context.Context, resource *Resource, stateData *ResourceModel) err
 func Create(ctx context.Context, resource *Resource, planData *ResourceModel) error {
 
 	// do create
-	// e.g. createOutp, err := resource.client.CreateExample(ctx, ...)
-
 	createOut, err := resource.client.CreateCloudFmcDevice(ctx, cloudfmc.NewCreateInput())
 	if err != nil {
 		return err
 	}
 
 	// map response to terraform types
-	// e.g. planData.ID = types.StringValue(createOutp.Uid)
 	planData.Id = types.StringValue(createOut.Uid)
 	planData.Name = types.StringValue(createOut.Name)
+	planData.Hostname = types.StringValue(createOut.Host)
 
 	return nil
 }

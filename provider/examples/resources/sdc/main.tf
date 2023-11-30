2e62ca0f5ab5031010a948de35db8fe60edbccca
Weilue Luo <weilluo@cisco.com>
2023-11-30 21:51:56 +0000

    add example

---
+++ provider/examples/resources/sdc/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "github.com/CiscoDevNet/cdo-automation//modules/aws_vpc"
+}
+
+
+module "sdc_in_aws" {
+  source    = "github.com/CiscoDevNet/cdo-automation//modules/cdo/sdc-aws"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.private_subnet_id
+}
+
+
+output "sdc_name" {
+  value = module.sdc_in_aws.sdc_name
+}

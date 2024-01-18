dd86e261c6be2d16267e2f38f40c6132cc259d4b
Weilue Luo <weilluo@cisco.com>
2024-01-18 10:51:50 +0000

    impl

--- provider/internal/cdfmc/operation.go
+++ provider/internal/cdfmc/operation.go
@@ -13,11 +13,16 @@ func Read(ctx context.Context, resource *Resource, stateData *ResourceModel) err
 	if err != nil {
 		return err
 	}
+	cloudFmcSpecificDeviceReadOut, err := resource.client.ReadCloudFmcSpecificDevice(ctx, cloudfmc.NewReadSpecificInput(readOut.Uid))
+	if err != nil {
+		return err
+	}
 
 	// map response to terraform types
 	stateData.Id = types.StringValue(readOut.Uid)
 	stateData.Name = types.StringValue(readOut.Name)
 	stateData.Hostname = types.StringValue(readOut.Host)
+	stateData.DomainUuid = types.StringValue(cloudFmcSpecificDeviceReadOut.DomainUid)
 
 	return nil
 }
@@ -29,11 +34,16 @@ func Create(ctx context.Context, resource *Resource, planData *ResourceModel) er
 	if err != nil {
 		return err
 	}
+	cloudFmcSpecificDeviceReadOut, err := resource.client.ReadCloudFmcSpecificDevice(ctx, cloudfmc.NewReadSpecificInput(createOut.Uid))
+	if err != nil {
+		return err
+	}
 
 	// map response to terraform types
 	planData.Id = types.StringValue(createOut.Uid)
 	planData.Name = types.StringValue(createOut.Name)
 	planData.Hostname = types.StringValue(createOut.Host)
+	planData.DomainUuid = types.StringValue(cloudFmcSpecificDeviceReadOut.DomainUid)
 
 	return nil
 }
